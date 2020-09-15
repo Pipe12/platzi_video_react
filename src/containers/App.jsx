3e81cdcb4b853f672b7841b5d2d65114aa93250b
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-09-15 15:12:41 -0500

    Create and add styles to Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
@@ -19,6 +20,8 @@ const App = () => (
         <CarouselItem />
       </Carousel>
     </Categories>
+
+    <Footer />
   </div>
 );
 
