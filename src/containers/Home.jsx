518e3dadc9a69f9b73b724aa15c6f88164838d3f
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-09-30 11:34:03 -0500

    Install React Router and create a root path

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -46,4 +46,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
