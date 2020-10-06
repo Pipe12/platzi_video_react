5039adbe5f03ffbc7f8e23ec14ba9f1bb121d601
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-10-06 16:14:57 -0500

    --amend

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
