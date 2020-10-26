0433ca90a39d8c432d9c961c580bd16ad8936186
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-10-26 13:27:31 -0500

    load user login info to state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,6 +13,11 @@ const reducer = (state, action) => {
         ...state,
         myList: state.myList.filter((items) => items.id !== action.payload),
       };
+    case 'LOGIN_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }
