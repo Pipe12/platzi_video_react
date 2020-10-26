f2703fd07d2fe55dec7cd5069e2c3bcd3ec566c2
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-10-26 17:54:39 -0500

    Simulate login and logout actions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import avatarIcon from '../assets/static/avatar_icon.png';
@@ -11,8 +13,8 @@ const Header = (props) => {
   const hasUser = (Object.keys(user).length > 0);
 
   const handleLogout = () => {
-    
-  }
+    props.logoutRequest({});
+  };
 
   return (
     <header className='header'>
@@ -31,22 +33,22 @@ const Header = (props) => {
         <ul>
           {
             hasUser ?
-              <li><a href='/'>Cuenta</a></li> :
+              <li><a href='/'>{user.name}</a></li> :
               null
           }
           {
             hasUser ?
-              <li><a href='#logout' onClick={handleLogout}>Cuenta</a></li> :
-              <li>
-                <Link to='/login'>
-                  Iniciar seción
-                </Link>
-              </li>
+              <li><a href='#logout' onClick={handleLogout}>Cerrar sesión</a></li> :
+              <li><Link to='/login'>Iniciar sesión</Link></li>
           }
         </ul>
       </div>
     </header>
   );
+};
+
+Header.propTypes = {
+  user: PropTypes.object,
 };
 
 const mapStateToProps = (state) => {
@@ -55,4 +57,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,6 +18,11 @@ const reducer = (state, action) => {
         ...state,
         user: action.payload,
       };
+    case 'LOGOUT_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }
