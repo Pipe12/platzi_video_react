3c70ea5818db1e64b9447c75509316bbd855e3fe
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-10-29 12:00:06 -0500

    Add functionality go_back button and config a bundle.js at proyect root in webpack

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => {
-
+const Player = (props) => {
+  const { id } = props.match.params;
+  console.log(id);
   return (
     <div className='player'>
       <video controls autoPlay>
         <source src='' type='video/mp4' />
       </video>
       <div className='player-back'>
-        <button type='button'>
+        <button type='button' onClick={() => props.history.goBack()}>
           Regresar
         </button>
       </div>
